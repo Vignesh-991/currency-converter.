e0c2bfeff5341d26f0c30d4fd11cfb907d32eaff
Vignesh-991 <149889296+Vignesh-991@users.noreply.github.com>
2025-08-13 17:29:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const BASE_URL =
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+let dropdowns = document.querySelectorAll(".dropdown select");
+let btn = document.querySelector("form button");
+let fromCurr = document.querySelector(".from select");
+let toCurr = document.querySelector(".to select");
+let msg = document.querySelector(".msg");
+for (let select of dropdowns) {
+
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === 'USD') {
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode == "INR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+const updateFlag = (e) => {
+    console.log(e);
+    let currCode = e.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = document.querySelector("img");
+    img.src = newSrc;
+}
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    let URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;//`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    //console.log(response);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    let final = (amtVal * rate).toFixed(2);
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${final} ${toCurr.value}`;
+}
+
+
